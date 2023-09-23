429d12d0e757eec3aba34b65069fbc52f0c38615
lia715 <sy0715p@naver.com>
2023-09-23 16:07:38 +0900

    23.09.23 링커리어 채용 페이지 불러오기

--- src/pages/Blank5.jsx
+++ src/pages/Blank5.jsx
@@ -2,25 +2,13 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Blank5 = () => {
-  const [naverTitle, setNaverTitle] = useState('');
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get(''); // 서버 호출
-        setNaverTitle(response.data.pageTitle);
-      } catch (error) {
-        console.error('Error while fetching data:', error);
-      }
-    }
-
-    fetchData();
-  }, []);
-
   return (
     <div>
-      <h1>This is Blank page5</h1>
-      <p>Naver Page Title: {naverTitle}</p>
+      <iframe
+        src="https://linkareer.com/list/recruit?filterBy_activityTypeID=5&filterBy_status=OPEN&orderBy_direction=DESC&orderBy_field=RECENT&page=1"
+        width="80%"
+        height="500"
+      ></iframe>
     </div>
   );
 };
